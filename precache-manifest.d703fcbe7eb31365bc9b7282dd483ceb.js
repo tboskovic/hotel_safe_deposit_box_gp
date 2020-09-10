979556f1a1a478ab405eaaa9913e0963185ad896
tijana.boskovic.riitech <tboskovicc@gmail.com>
2020-09-10 11:55:58 +0200

    Updates

---
+++ precache-manifest.d703fcbe7eb31365bc9b7282dd483ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a1d94530ba47244e9f90036020ab02",
+    "url": "/hotel_safe_deposit_box_gp/index.html"
+  },
+  {
+    "revision": "1e23ed72ae08eb583735",
+    "url": "/hotel_safe_deposit_box_gp/static/css/main.482d8b82.chunk.css"
+  },
+  {
+    "revision": "a5f330c261a4a27d30d8",
+    "url": "/hotel_safe_deposit_box_gp/static/js/2.717790c8.chunk.js"
+  },
+  {
+    "revision": "567ff855e89811c94bb38895efc27afb",
+    "url": "/hotel_safe_deposit_box_gp/static/js/2.717790c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e23ed72ae08eb583735",
+    "url": "/hotel_safe_deposit_box_gp/static/js/main.f4ff1700.chunk.js"
+  },
+  {
+    "revision": "d818e2e4c6470dcc227d",
+    "url": "/hotel_safe_deposit_box_gp/static/js/runtime-main.729abc87.js"
+  }
+]);
